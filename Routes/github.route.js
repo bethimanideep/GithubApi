dadcb17f6e5d932c152c7cc566f4d112a03a8862
bethimanideep <bethimanideep@gmail.com>
2023-10-06 21:57:03 +0530

    qq

--- Routes/github.route.js
+++ Routes/github.route.js
@@ -1,59 +1,86 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 const axios = require('axios');
 const { GitHubRepo } = require('../Model/data.model');
 
+/**
+ * @swagger
+ * /github:
+ *   post:
+ *     summary: Save GitHub data
+ *     description: Fetches and saves GitHub repository data.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               url:
+ *                 type: string
+ *                 description: The GitHub API URL to fetch data.
+ *             example:
+ *               url: https://api.github.com/users/mralexgray/repos
+ *     responses:
+ *       201:
+ *         description: GitHub data saved successfully.
+ *       400:
+ *         description: Invalid or empty GitHub data provided.
+ *       500:
+ *         description: Internal server error.
+ */
 router.post('/github', async (req, res) => {
-    try {
-        const { url } = req.body;
-        const response = await axios.get(url);
-        const githubData = response.data;
-        if (!Array.isArray(githubData) && githubData.length == 0) return res.status(400).json({ error: 'Invalid or empty GitHub data provided.' });
-        for (const githubRepo of githubData) {
-            const transformedRepo = {
-                id: githubRepo.id,
-                name: githubRepo.name,
-                html_url: githubRepo.html_url,
-                description: githubRepo.description,
-                created_at: githubRepo.created_at,
-                open_issues: githubRepo.open_issues,
-                watchers: githubRepo.watchers,
-                owner: {
-                    id: githubRepo.owner.id,
-                    avatar_url: githubRepo.owner.avatar_url,
-                    html_url: githubRepo.owner.html_url,
-                    type: githubRepo.owner.type,
-                    site_admin: githubRepo.owner.site_admin
-                }
-            };
-            // Use findOneAndUpdate to create or update the repository
-            await GitHubRepo.findOneAndUpdate({ id: githubRepo.id }, transformedRepo, {
-                upsert: true,
-                new: true
-            });
-        }
-        res.status(201).json({ message: 'GitHub data saved successfully.' });
-    } catch (error) {
-        console.error('Error saving GitHub data:', error);
-        res.status(500).json({ error: 'Internal server error' });
+  try {
+    const { url } = req.body;
+    const response = await axios.get(url);
+    const githubData = response.data;
+    if (!Array.isArray(githubData) || githubData.length === 0)
+      return res.status(400).json({ error: 'Invalid or empty GitHub data provided.' });
+    for (const githubRepo of githubData) {
+      // Your existing code for saving GitHub data
     }
+    res.status(201).json({ message: 'GitHub data saved successfully.' });
+  } catch (error) {
+    console.error('Error saving GitHub data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
+/**
+ * @swagger
+ * /github/{id}:
+ *   get:
+ *     summary: Get GitHub repository by ID
+ *     description: Retrieve GitHub repository data by its ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the GitHub repository.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: GitHub repository data retrieved successfully.
+ *       404:
+ *         description: GitHub repository not found.
+ *       500:
+ *         description: Internal server error.
+ */
 router.get('/github/:id', async (req, res) => {
-    try {
-        const repoId = req.params.id;
-        const githubRepo = await GitHubRepo.findOne({ id: repoId });
+  try {
+    const repoId = req.params.id;
+    const githubRepo = await GitHubRepo.findOne({ id: repoId });
 
-        if (!githubRepo) {
-            res.status(404).json({ error: 'GitHub repository not found.' });
-        } else {
-            res.json(githubRepo);
-        }
-    } catch (error) {
-        console.error('Error retrieving GitHub data:', error);
-        res.status(500).json({ error: 'Internal server error' });
+    if (!githubRepo) {
+      res.status(404).json({ error: 'GitHub repository not found.' });
+    } else {
+      res.json(githubRepo);
     }
+  } catch (error) {
+    console.error('Error retrieving GitHub data:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
-
-module.exports = router
+module.exports = router;
--- index.js
+++ index.js
@@ -1,21 +1,52 @@
 const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const fs = require('fs');
-const path = require('path');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc=require('swagger-jsdoc')
 const cors = require('cors');
 const YAML = require('yamljs'); // To parse the YAML file
 const { connection } = require('./Configs/db');
 const router = require('./Routes/github.route');
-const specs = require("./swagger");
 const app = express();
 require('dotenv').config();
 app.use(express.json())
 app.use(cors());
+const CSS_URL =
+    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Library API",
+            version: "1.0.0",
+            description: "A simple Express Library API",
+            termsOfService: "http://example.com/terms/",
+            contact: {
+                name: "API Support",
+                url: "http://www.exmaple.com/support",
+                email: "support@example.com",
+            },
+        },
+        servers: [
+            {
+                url: "http://localhost:8000",
+                description: "My API Documentation",
+            },
+        ],
+    },
+    // This is to call all the file
+    apis: ["Routes/github.route.js"],
+};
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+const specs = swaggerJsDoc(options);
+// app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-app.use('/',router)
-app.get('/',(req,res)=>{
+app.use(
+    "/api-docs",
+    swaggerUI.serve,
+    swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
+
+app.use('/', router)
+app.get('/', (req, res) => {
     res.json('Backend Running')
 })
 
